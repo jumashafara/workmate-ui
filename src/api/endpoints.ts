26cad1b407647ce6ea2bb111534b7a12135b9c21
jumashafara <jumashafara0@gmail.com>
2025-03-24 07:34:45 -0400

    workflow: updated api base url

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,5 +1,6 @@
-const API_ENDPOINT = 'http://localhost:8000/api'
-const ACCOUNTS_ENDPOINT = 'http://localhost:8000/accounts'
+const API_BASE_URL = 'https://workmate.api.dataidea.org'
+const API_ENDPOINT = `${API_BASE_URL}/api`
+const ACCOUNTS_ENDPOINT = `${API_BASE_URL}/accounts`
 
 // export
-export {API_ENDPOINT, ACCOUNTS_ENDPOINT}
+export {API_BASE_URL, API_ENDPOINT, ACCOUNTS_ENDPOINT}
