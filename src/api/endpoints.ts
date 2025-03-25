6b85b9171330638ecf6fd2c0d30aad1a8e3e9a34
jumashafara <jumashafara0@gmail.com>
2025-03-25 11:10:03 -0400

    fix: updated endpoint

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,5 +1,5 @@
-// const API_BASE_URL = 'https://workmate.api.dataidea.org'
-const API_BASE_URL = 'http://localhost:8000'
+const API_BASE_URL = 'https://workmate.api.dataidea.org'
+// const API_BASE_URL = 'http://localhost:8000'
 const API_ENDPOINT = `${API_BASE_URL}/api`
 const ACCOUNTS_ENDPOINT = `${API_BASE_URL}/accounts`
 
