359ad64524cf2b3a2f717f863854a19d0a06b7b6
wcosmas <wcosmas75@gmail.com>
2025-07-30 17:35:46 +0300

    Update AppSidebar: add group class to SidebarMenuButton and adjust logo size for improved responsiveness and visibility.

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -143,9 +143,9 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarHeader>
         <SidebarMenuButton
           size="lg"
-          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+          className="group data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
         >
-          <div className=" text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+          <div className=" text-sidebar-primary-foreground flex aspect-square size-12 items-center justify-center rounded-lg transition-all group-data-[state=collapsed]:size-8">
             <Image
               src="/RTV_Logo.png"
               alt="RTV Logo"
